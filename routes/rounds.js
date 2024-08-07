2d086b36c85f9f498ab60bfe0a80b37ac9991bf0
giska1923 <ognjen97markovic@gmail.com>
2024-08-07 21:21:13 +0200

    allRounds json

--- routes/rounds.js
+++ routes/rounds.js
@@ -1,18 +1,23 @@
 import express from 'express';
 import api from '../api.js';
 import appConfig from '../config.js';
-import { getAllPages, mapTypeIdsToNames } from '../helpers/api.js';
+import {
+  getAllPages,
+  mapTypeIdsToNames,
+  flattenNestedArray,
+} from '../helpers/api.js';
 
 const router = express.Router();
 
 router.get('/', async (_, res) => {
   try {
     const data = await getAllPages(
-      `rounds/?api_token=${appConfig.API_TOKEN}&includes=fixtures.events&per_page=${appConfig.PER_PAGE}`
+      `rounds/?api_token=${appConfig.API_TOKEN}&per_page=${appConfig.PER_PAGE}`
     );
 
     mapTypeIdsToNames(data);
-    res.json(data);
+    const flattenedArray = flattenNestedArray(data);
+    res.json(flattenedArray);
   } catch (err) {
     console.log(err.message);
     res.status(500).send({ error: err.message });
@@ -26,6 +31,20 @@ router.get('/page', async (_, res) => {
     );
     const data = response.data;
 
+    res.json(data);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send({ error: err.message });
+  }
+});
+
+router.get('/fixtures/events', async (_, res) => {
+  try {
+    const data = await getAllPages(
+      `rounds/?api_token=${appConfig.API_TOKEN}&includes=fixtures.events&per_page=${appConfig.PER_PAGE}`
+    );
+
+    mapTypeIdsToNames(data);
     res.json(data);
   } catch (err) {
     console.log(err.message);
