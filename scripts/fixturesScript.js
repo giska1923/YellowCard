f4ee39ef2cdcb47f87e7b12f95aceacbfa6d90bf
giska1923 <ognjen97markovic@gmail.com>
2024-08-14 19:31:45 +0200

    revert switch from league, season, team ids to names

--- scripts/fixturesScript.js
+++ scripts/fixturesScript.js
@@ -1,22 +1,13 @@
-import {
-  calc,
-  getLeagueIdByName,
-  getSeasonIdByNameAndLeague,
-  getTeamIdByName,
-} from './fixtures.js';
+import { calc } from './fixtures.js';
 
 // Function to handle the average goals calculation
 function calcAverage() {
   console.log('calcAverage - Button clicked');
 
   const resultElement = document.getElementById('result');
-  const leagueName = document.getElementById('league').value;
-  const seasonName = document.getElementById('season').value;
-  const teamName = document.getElementById('team').value;
-
-  const leagueId = getLeagueIdByName(leagueName);
-  const seasonId = getSeasonIdByNameAndLeague(seasonName, leagueId);
-  const teamId = getTeamIdByName(teamName);
+  const leagueId = document.getElementById('league').value;
+  const seasonId = document.getElementById('season').value;
+  const teamId = document.getElementById('team').value;
 
   const duration = document.getElementsByName('duration');
   let matchDuration = 0;
