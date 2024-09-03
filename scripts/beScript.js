1995f519e8df2bfd34c895640ee5f430f36000ed
giska1923 <ognjen97markovic@gmail.com>
2024-09-03 20:28:32 +0200

    swap back be & goals

--- scripts/beScript.js
+++ scripts/beScript.js
@@ -1,64 +1,174 @@
-import {
-  calc,
-  getAllLeagues,
-  filterSeasonsByLeagueId,
-  getParticipants,
-} from './goals.js';
+import { filterMatches, getAllLeagues } from './goals.js';
 
-// Function to handle the average goals calculation
-function calcAverage() {
-  console.log('calcAverage - Button clicked');
+const getNumValue = inputVal => {
+  return inputVal === '' ? undefined : Number(inputVal);
+};
 
-  const resultElement = document.getElementById('result');
-  const leagueId = document.getElementById('league').value;
-  const seasonId = document.getElementById('season').value;
-  const teamId = document.getElementById('team').value;
+function beAlgo() {
+  console.log('beAlgo - Button clicked');
 
-  const exactNumGoals =
-    document.getElementById('exactNumGoals').value === ''
-      ? -1
-      : Number(document.getElementById('exactNumGoals').value);
+  const resultElement = document.getElementById('beResult');
+  const leagueId = getNumValue(document.getElementById('beLeague').value);
 
-  let result;
-
-  console.log('leagueId, seasonId, teamId: ', leagueId, seasonId, teamId);
-  if (leagueId && seasonId && teamId) {
-    result = calc(
-      Number(leagueId),
-      Number(seasonId),
-      Number(teamId),
-      exactNumGoals
-    );
+  // HARDCODED!
+  let seasonId;
+  if (leagueId === 271) {
+    seasonId = 21644;
   } else {
-    result = calc();
+    seasonId = 21787;
   }
 
+  const avgGoalsInGeneralCondition = document.getElementById('beAvgComp').value;
+  const avgGoalsInGeneral = getNumValue(document.getElementById('beAvg').value);
+
+  const avgGoalsHomeCondition = document.getElementById('beAvgHomeComp').value;
+  const avgGoalsHome = getNumValue(document.getElementById('beAvgHome').value);
+  let avgScoredHomeCondition;
+  let avgScoredHome;
+  let avgConcededHomeCondition;
+  let avgConcededHome;
+  if (document.getElementById('beAvgHomeSCOpt').value === 'scored') {
+    avgScoredHomeCondition = document.getElementById('beAvgHomeSCComp').value;
+    avgScoredHome = getNumValue(document.getElementById('beAvgHomeSC').value);
+  } else {
+    avgConcededHomeCondition = document.getElementById('beAvgHomeSCComp').value;
+    avgConcededHome = getNumValue(document.getElementById('beAvgHomeSC').value);
+  }
+  let avg1HHomeCondition;
+  let avg1HHome;
+  let avg2HHomeCondition;
+  let avg2HHome;
+  if (document.getElementById('beAvgHomeHalf').value === '1st half') {
+    avg1HHomeCondition = document.getElementById('beAvgHomeHalfComp').value;
+    avg1HHome = getNumValue(
+      document.getElementById('beAvgHomeHalfGoals').value
+    );
+  } else {
+    avg2HHomeCondition = document.getElementById('beAvgHomeHalfComp').value;
+    avg2HHome = getNumValue(
+      document.getElementById('beAvgHomeHalfGoals').value
+    );
+  }
+
+  const avgGoalsAwayCondition = document.getElementById('beAvgAwayComp').value;
+  const avgGoalsAway = getNumValue(document.getElementById('beAvgAway').value);
+  let avgScoredAwayCondition;
+  let avgScoredAway;
+  let avgConcededAwayCondition;
+  let avgConcededAway;
+  if (document.getElementById('beAvgAwaySCOpt').value === 'scored') {
+    avgScoredAwayCondition = document.getElementById('beAvgAwaySCComp').value;
+    avgScoredAway = getNumValue(document.getElementById('beAvgAwaySC').value);
+  } else {
+    avgConcededAwayCondition = document.getElementById('beAvgAwaySCComp').value;
+    avgConcededAway = getNumValue(document.getElementById('beAvgAwaySC').value);
+  }
+  let avg1HAwayCondition;
+  let avg1HAway;
+  let avg2HAwayCondition;
+  let avg2HAway;
+  if (document.getElementById('beAvgAwayHalf').value === '1st half') {
+    avg1HAwayCondition = document.getElementById('beAvgAwayHalfComp').value;
+    avg1HAway = getNumValue(
+      document.getElementById('beAvgAwayHalfGoals').value
+    );
+  } else {
+    avg2HAwayCondition = document.getElementById('beAvgAwayHalfComp').value;
+    avg2HAway = getNumValue(
+      document.getElementById('beAvgAwayHalfGoals').value
+    );
+  }
+
+  let desiredAvgGoalsInGeneral;
+  let desiredAvgGoalsInGeneralCondition;
+  let desiredAvgGoalsHome;
+  let desiredAvgGoalsHomeCondition;
+  let desiredAvgGoalsAway;
+  let desiredAvgGoalsAwayCondition;
+  let desiredAvg1H;
+  let desiredAvg1HCondition;
+  let desiredAvg2H;
+  let desiredAvg2HCondition;
+  if (
+    document.getElementById('beDesiredCriteria').value ===
+    'Total number of goals'
+  ) {
+    desiredAvgGoalsInGeneralCondition =
+      document.getElementById('beDesiredComp').value;
+    desiredAvgGoalsInGeneral = getNumValue(
+      document.getElementById('beDesiredGoals').value
+    );
+  } else if (
+    document.getElementById('beDesiredCriteria').value === '1st Half'
+  ) {
+    desiredAvg1HCondition = document.getElementById('beDesiredComp').value;
+    desiredAvg1H = getNumValue(document.getElementById('beDesiredGoals').value);
+  } else if (
+    document.getElementById('beDesiredCriteria').value === '2nd Half'
+  ) {
+    desiredAvg2HCondition = document.getElementById('beDesiredComp').value;
+    desiredAvg2H = getNumValue(document.getElementById('beDesiredGoals').value);
+  } else if (document.getElementById('beDesiredCriteria').value === 'Home') {
+    desiredAvgGoalsHomeCondition =
+      document.getElementById('beDesiredComp').value;
+    desiredAvgGoalsHome = getNumValue(
+      document.getElementById('beDesiredGoals').value
+    );
+  } else {
+    desiredAvgGoalsAwayCondition =
+      document.getElementById('beDesiredComp').value;
+    desiredAvgGoalsAway = getNumValue(
+      document.getElementById('beDesiredGoals').value
+    );
+  }
+
+  console.log('leagueId: ', leagueId);
+  const options = {
+    avgGoalsInGeneral: avgGoalsInGeneral,
+    avgGoalsInGeneralCondition: avgGoalsInGeneralCondition,
+    avgGoalsHome: avgGoalsHome,
+    avgGoalsHomeCondition: avgGoalsHomeCondition,
+    avgGoalsAway: avgGoalsAway,
+    avgGoalsAwayCondition: avgGoalsAwayCondition,
+    avgScoredHome: avgScoredHome,
+    avgScoredHomeCondition: avgScoredHomeCondition,
+    avgConcededHome: avgConcededHome,
+    avgConcededHomeCondition: avgConcededHomeCondition,
+    avgScoredAway: avgScoredAway,
+    avgScoredAwayCondition: avgScoredAwayCondition,
+    avgConcededAway: avgConcededAway,
+    avgConcededAwayCondition: avgConcededAwayCondition,
+    avg1HHome: avg1HHome,
+    avg1HHomeCondition: avg1HHomeCondition,
+    avg2HHome: avg2HHome,
+    avg2HHomeCondition: avg2HHomeCondition,
+    avg1HAway: avg1HAway,
+    avg1HAwayCondition: avg1HAwayCondition,
+    avg2HAway: avg2HAway,
+    avg2HAwayCondition: avg2HAwayCondition,
+  };
+  const desiredOutcomeOptions = {
+    desiredAvgGoalsInGeneral: desiredAvgGoalsInGeneral,
+    desiredAvgGoalsInGeneralCondition: desiredAvgGoalsInGeneralCondition,
+    desiredAvgGoalsHome: desiredAvgGoalsHome,
+    desiredAvgGoalsHomeCondition: desiredAvgGoalsHomeCondition,
+    desiredAvgGoalsAway: desiredAvgGoalsAway,
+    desiredAvgGoalsAwayCondition: desiredAvgGoalsAwayCondition,
+    desiredAvg1H: desiredAvg1H,
+    desiredAvg1HCondition: desiredAvg1HCondition,
+    desiredAvg2H: desiredAvg2H,
+    desiredAvg2HCondition: desiredAvg2HCondition,
+  };
+
+  const result = filterMatches(
+    leagueId,
+    seasonId,
+    options,
+    desiredOutcomeOptions
+  );
+
   if (result) {
-    document.getElementById(
-      'calc'
-    ).innerText = `Matches count: ${result.count} \n
-        Goals count: ${result.totalGoals} <> Average: ${result.averageGoals} \n
-        Goals scored count: ${result.totalGoalsScored} <> Average: ${result.averageGoalsScored} \n
-        Goals scored count home: ${result.totalGoalsScoredHome} <> Average: ${result.averageGoalsScoredHome} \n
-        Goals scored count home 1st half: ${result.firstHalfGoalsScoredHome} <> Average: ${result.averageFirstHalfGoalsScoredHome} \n
-        Goals scored count home 2nd half: ${result.secondHalfGoalsScoredHome} <> Average: ${result.averageSecondHalfGoalsScoredHome} \n
-        Goals scored count away: ${result.totalGoalsScoredAway} <> Average: ${result.averageGoalsScoredAway} \n
-        Goals scored count away 1st half: ${result.firstHalfGoalsScoredAway} <> Average: ${result.averageFirstHalfGoalsScoredAway} \n
-        Goals scored count away 2nd half: ${result.secondHalfGoalsScoredAway} <> Average: ${result.averageSecondHalfGoalsScoredAway} \n
-        Goals conceded count: ${result.totalGoalsConceded} <> Average: ${result.averageGoalsConceded} \n
-        Goals conceded count home: ${result.totalGoalsConcededHome} <> Average: ${result.averageGoalsConcededHome} \n
-        Goals conceded count home 1st half: ${result.firstHalfGoalsConcededHome} <> Average: ${result.averageFirstHalfGoalsConcededHome} \n
-        Goals conceded count home 2nd half: ${result.secondHalfGoalsConcededHome} <> Average: ${result.averageSecondHalfGoalsConcededHome} \n
-        Goals conceded count away: ${result.totalGoalsConcededAway} <> Average: ${result.averageGoalsConcededAway} \n
-        Goals conceded count away 1st half: ${result.firstHalfGoalsConcededAway} <> Average: ${result.averageFirstHalfGoalsConcededAway} \n
-        Goals conceded count away 2nd half: ${result.secondHalfGoalsConcededAway} <> Average: ${result.averageSecondHalfGoalsConcededAway} \n
-        Corners count: ${result.totalCorners} <> Average: ${result.averageCorners} \n
-        Yellow cards count: ${result.totalYellowCards} <> Average: ${result.averageYellowCards} \n
-        Red cards count: ${result.totalRedCards} <> Average: ${result.averageRedCards} \n
-        Win ratio: ${result.averageWinRatio}% \n
-        Lose ratio: ${result.averageLoseRatio}% \n
-        Draw ratio: ${result.averageDrawRatio}% \n
-        Percent of matches with scored entered goals: ${result.averageExactNumGoals}%`;
+    resultElement.innerText = `${result} %`;
     resultElement.classList.remove('inactive');
   } else {
     resultElement.classList.add('inactive');
@@ -66,23 +176,10 @@ function calcAverage() {
 }
 
 // Attach event listener to the button
-document.getElementById('calcGoalsBtn').addEventListener('click', calcAverage);
-
-// JavaScript to handle tab switching
-document.querySelectorAll('.sidebar a').forEach(tab => {
-  tab.addEventListener('click', function () {
-    document
-      .querySelectorAll('.sidebar a')
-      .forEach(link => link.classList.remove('active'));
-    this.classList.add('active');
-  });
-});
+document.getElementById('beCalcBtn').addEventListener('click', beAlgo);
 
 document.addEventListener('DOMContentLoaded', () => {
-  const seasonSelect = document.getElementById('season');
-  const leagueSelect = document.getElementById('league');
-  const teamSelect = document.getElementById('team');
-
+  const leagueSelect = document.getElementById('beLeague');
   const defaultLeagueId = 271;
 
   function populateSelectBox(selectElement, items, valueKey, textKey) {
@@ -95,43 +192,11 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  function populateSeasonSelect(seasons) {
-    populateSelectBox(seasonSelect, seasons, 'id', 'name');
-    if (seasons.length > 0) {
-      seasonSelect.value = seasons[0].id;
-      populateTeamSelect(defaultLeagueId, seasons[0].id);
-    }
-  }
-
-  function populateTeamSelect(leagueId, seasonId) {
-    const uniqueParticipants = getParticipants(leagueId, seasonId);
-    populateSelectBox(teamSelect, uniqueParticipants, 'id', 'name');
-    if (uniqueParticipants.length > 0) {
-      teamSelect.value = uniqueParticipants[0].id;
-    }
-  }
-
   function init() {
     try {
       const allLeagues = getAllLeagues();
       populateSelectBox(leagueSelect, allLeagues, 'id', 'name');
       leagueSelect.value = defaultLeagueId;
-
-      const initialSeasons = filterSeasonsByLeagueId(defaultLeagueId);
-      populateSeasonSelect(initialSeasons);
-
-      leagueSelect.addEventListener('change', () => {
-        const selectedLeagueId = leagueSelect.value;
-        const filteredSeasons = filterSeasonsByLeagueId(selectedLeagueId);
-        populateSeasonSelect(filteredSeasons);
-        populateTeamSelect(selectedLeagueId, filteredSeasons[0].id);
-      });
-
-      seasonSelect.addEventListener('change', () => {
-        const selectedSeasonId = seasonSelect.value;
-        const selectedLeagueId = leagueSelect.value;
-        populateTeamSelect(selectedLeagueId, selectedSeasonId);
-      });
     } catch (error) {
       console.error('Error initializing page:', error);
     }
