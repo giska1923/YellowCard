ee5de76ac4c4be6a03d379b746e0633f0834700d
giska1923 <ognjen97markovic@gmail.com>
2024-09-12 18:50:17 +0200

    minor code fixes

--- scripts/beScript.js
+++ scripts/beScript.js
@@ -4,164 +4,201 @@ const getNumValue = inputVal => {
   return inputVal === '' ? undefined : Number(inputVal);
 };
 
+const getScoredOrConcededOptions = (option, comp, value) => {
+  if (option === 'scored') {
+    return [comp, value, undefined, undefined];
+  } else {
+    return [undefined, undefined, comp, value];
+  }
+};
+
+const getHalfGoalsOptions = (half, comp, goals) => {
+  if (half === '1st half') {
+    return [comp, goals, undefined, undefined];
+  } else {
+    return [undefined, undefined, comp, goals];
+  }
+};
+
+const getHomeTeamOptions = elements => {
+  const [
+    avgScoredHomeCondition,
+    avgScoredHome,
+    avgConcededHomeCondition,
+    avgConcededHome,
+  ] = getScoredOrConcededOptions(
+    elements.avgHomeSCOpt,
+    elements.avgHomeSCComp,
+    elements.avgHomeSC
+  );
+
+  const [avg1HHomeCondition, avg1HHome, avg2HHomeCondition, avg2HHome] =
+    getHalfGoalsOptions(
+      elements.avgHomeHalf,
+      elements.avgHomeHalfComp,
+      elements.avgHomeHalfGoals
+    );
+
+  return {
+    avgGoalsH: elements.avgH,
+    avgGoalsHCondition: elements.avgHComp,
+    avgGoalsHome: elements.avgHome,
+    avgGoalsHomeCondition: elements.avgHomeComp,
+    avgScoredHome,
+    avgScoredHomeCondition,
+    avgConcededHome,
+    avgConcededHomeCondition,
+    avg1HHome,
+    avg1HHomeCondition,
+    avg2HHome,
+    avg2HHomeCondition,
+  };
+};
+
+const getAwayTeamOptions = elements => {
+  const [
+    avgScoredAwayCondition,
+    avgScoredAway,
+    avgConcededAwayCondition,
+    avgConcededAway,
+  ] = getScoredOrConcededOptions(
+    elements.avgAwaySCOpt,
+    elements.avgAwaySCComp,
+    elements.avgAwaySC
+  );
+
+  const [avg1HAwayCondition, avg1HAway, avg2HAwayCondition, avg2HAway] =
+    getHalfGoalsOptions(
+      elements.avgAwayHalf,
+      elements.avgAwayHalfComp,
+      elements.avgAwayHalfGoals
+    );
+
+  return {
+    avgGoalsA: elements.avgA,
+    avgGoalsACondition: elements.avgAComp,
+    avgGoalsAway: elements.avgAway,
+    avgGoalsAwayCondition: elements.avgAwayComp,
+    avgScoredAway,
+    avgScoredAwayCondition,
+    avgConcededAway,
+    avgConcededAwayCondition,
+    avg1HAway,
+    avg1HAwayCondition,
+    avg2HAway,
+    avg2HAwayCondition,
+  };
+};
+
+const getOptions = elements => {
+  const homeOptions = getHomeTeamOptions(elements);
+  const awayOptions = getAwayTeamOptions(elements);
+
+  return {
+    avgGoalsH: homeOptions.avgGoalsH,
+    avgGoalsHCondition: homeOptions.avgGoalsHCondition,
+    avgGoalsA: awayOptions.avgGoalsA,
+    avgGoalsACondition: awayOptions.avgGoalsACondition,
+    avgGoalsHome: homeOptions.avgGoalsHome,
+    avgGoalsHomeCondition: homeOptions.avgGoalsHomeCondition,
+    avgGoalsAway: awayOptions.avgGoalsAway,
+    avgGoalsAwayCondition: awayOptions.avgGoalsAwayCondition,
+    avgScoredHome: homeOptions.avgScoredHome,
+    avgScoredHomeCondition: homeOptions.avgScoredHomeCondition,
+    avgConcededHome: homeOptions.avgConcededHome,
+    avgConcededHomeCondition: homeOptions.avgConcededHomeCondition,
+    avgScoredAway: awayOptions.avgScoredAway,
+    avgScoredAwayCondition: awayOptions.avgScoredAwayCondition,
+    avgConcededAway: awayOptions.avgConcededAway,
+    avgConcededAwayCondition: awayOptions.avgConcededAwayCondition,
+    avg1HHome: homeOptions.avg1HHome,
+    avg1HHomeCondition: homeOptions.avg1HHomeCondition,
+    avg2HHome: homeOptions.avg2HHome,
+    avg2HHomeCondition: homeOptions.avg2HHomeCondition,
+    avg1HAway: awayOptions.avg1HAway,
+    avg1HAwayCondition: awayOptions.avg1HAwayCondition,
+    avg2HAway: awayOptions.avg2HAway,
+    avg2HAwayCondition: awayOptions.avg2HAwayCondition,
+  };
+};
+
+const getDesiredOutcomeOptions = elements => {
+  switch (elements.desiredCriteria) {
+    case 'Total number of goals':
+      return {
+        desiredAvgGoalsInGeneral: elements.desiredGoals,
+        desiredAvgGoalsInGeneralCondition: elements.desiredComp,
+      };
+    case '1st Half':
+      return {
+        desiredAvg1H: elements.desiredGoals,
+        desiredAvg1HCondition: elements.desiredComp,
+      };
+    case '2nd Half':
+      return {
+        desiredAvg2H: elements.desiredGoals,
+        desiredAvg2HCondition: elements.desiredComp,
+      };
+    case 'Home':
+      return {
+        desiredAvgGoalsHome: elements.desiredGoals,
+        desiredAvgGoalsHomeCondition: elements.desiredComp,
+      };
+    case 'Away':
+    default:
+      return {
+        desiredAvgGoalsAway: elements.desiredGoals,
+        desiredAvgGoalsAwayCondition: elements.desiredComp,
+      };
+  }
+};
+
+const displayResult = (result, resultElement) => {
+  resultElement.innerText = result ? `${result} %` : '0%';
+  resultElement.classList.remove('inactive');
+};
+
 function beAlgo() {
   console.log('beAlgo - Button clicked');
 
-  const resultElement = document.getElementById('beResult');
-  const leagueId = getNumValue(document.getElementById('beLeague').value);
-
-  // HARDCODED!
-  let seasonId;
-  if (leagueId === 271) {
-    seasonId = 21644;
-  } else {
-    seasonId = 21787;
-  }
-
-  const avgGoalsHCondition = document.getElementById('beAvgHComp').value;
-  const avgGoalsH = getNumValue(document.getElementById('beAvgH').value);
-  const avgGoalsHomeCondition = document.getElementById('beAvgHomeComp').value;
-  const avgGoalsHome = getNumValue(document.getElementById('beAvgHome').value);
-  let avgScoredHomeCondition;
-  let avgScoredHome;
-  let avgConcededHomeCondition;
-  let avgConcededHome;
-  if (document.getElementById('beAvgHomeSCOpt').value === 'scored') {
-    avgScoredHomeCondition = document.getElementById('beAvgHomeSCComp').value;
-    avgScoredHome = getNumValue(document.getElementById('beAvgHomeSC').value);
-  } else {
-    avgConcededHomeCondition = document.getElementById('beAvgHomeSCComp').value;
-    avgConcededHome = getNumValue(document.getElementById('beAvgHomeSC').value);
-  }
-  let avg1HHomeCondition;
-  let avg1HHome;
-  let avg2HHomeCondition;
-  let avg2HHome;
-  if (document.getElementById('beAvgHomeHalf').value === '1st half') {
-    avg1HHomeCondition = document.getElementById('beAvgHomeHalfComp').value;
-    avg1HHome = getNumValue(
+  const elements = {
+    resultElement: document.getElementById('beResult'),
+    league: document.getElementById('beLeague').value,
+    avgHComp: document.getElementById('beAvgHComp').value,
+    avgH: getNumValue(document.getElementById('beAvgH').value),
+    avgHomeComp: document.getElementById('beAvgHomeComp').value,
+    avgHome: getNumValue(document.getElementById('beAvgHome').value),
+    avgHomeSCOpt: document.getElementById('beAvgHomeSCOpt').value,
+    avgHomeSCComp: document.getElementById('beAvgHomeSCComp').value,
+    avgHomeSC: getNumValue(document.getElementById('beAvgHomeSC').value),
+    avgHomeHalf: document.getElementById('beAvgHomeHalf').value,
+    avgHomeHalfComp: document.getElementById('beAvgHomeHalfComp').value,
+    avgHomeHalfGoals: getNumValue(
       document.getElementById('beAvgHomeHalfGoals').value
-    );
-  } else {
-    avg2HHomeCondition = document.getElementById('beAvgHomeHalfComp').value;
-    avg2HHome = getNumValue(
-      document.getElementById('beAvgHomeHalfGoals').value
-    );
-  }
-
-  const avgGoalsACondition = document.getElementById('beAvgAComp').value;
-  const avgGoalsA = getNumValue(document.getElementById('beAvgA').value);
-  const avgGoalsAwayCondition = document.getElementById('beAvgAwayComp').value;
-  const avgGoalsAway = getNumValue(document.getElementById('beAvgAway').value);
-  let avgScoredAwayCondition;
-  let avgScoredAway;
-  let avgConcededAwayCondition;
-  let avgConcededAway;
-  if (document.getElementById('beAvgAwaySCOpt').value === 'scored') {
-    avgScoredAwayCondition = document.getElementById('beAvgAwaySCComp').value;
-    avgScoredAway = getNumValue(document.getElementById('beAvgAwaySC').value);
-  } else {
-    avgConcededAwayCondition = document.getElementById('beAvgAwaySCComp').value;
-    avgConcededAway = getNumValue(document.getElementById('beAvgAwaySC').value);
-  }
-  let avg1HAwayCondition;
-  let avg1HAway;
-  let avg2HAwayCondition;
-  let avg2HAway;
-  if (document.getElementById('beAvgAwayHalf').value === '1st half') {
-    avg1HAwayCondition = document.getElementById('beAvgAwayHalfComp').value;
-    avg1HAway = getNumValue(
+    ),
+    avgAComp: document.getElementById('beAvgAComp').value,
+    avgA: getNumValue(document.getElementById('beAvgA').value),
+    avgAwayComp: document.getElementById('beAvgAwayComp').value,
+    avgAway: getNumValue(document.getElementById('beAvgAway').value),
+    avgAwaySCOpt: document.getElementById('beAvgAwaySCOpt').value,
+    avgAwaySCComp: document.getElementById('beAvgAwaySCComp').value,
+    avgAwaySC: getNumValue(document.getElementById('beAvgAwaySC').value),
+    avgAwayHalf: document.getElementById('beAvgAwayHalf').value,
+    avgAwayHalfComp: document.getElementById('beAvgAwayHalfComp').value,
+    avgAwayHalfGoals: getNumValue(
       document.getElementById('beAvgAwayHalfGoals').value
-    );
-  } else {
-    avg2HAwayCondition = document.getElementById('beAvgAwayHalfComp').value;
-    avg2HAway = getNumValue(
-      document.getElementById('beAvgAwayHalfGoals').value
-    );
-  }
-
-  let desiredAvgGoalsInGeneral;
-  let desiredAvgGoalsInGeneralCondition;
-  let desiredAvgGoalsHome;
-  let desiredAvgGoalsHomeCondition;
-  let desiredAvgGoalsAway;
-  let desiredAvgGoalsAwayCondition;
-  let desiredAvg1H;
-  let desiredAvg1HCondition;
-  let desiredAvg2H;
-  let desiredAvg2HCondition;
-  if (
-    document.getElementById('beDesiredCriteria').value ===
-    'Total number of goals'
-  ) {
-    desiredAvgGoalsInGeneralCondition =
-      document.getElementById('beDesiredComp').value;
-    desiredAvgGoalsInGeneral = getNumValue(
-      document.getElementById('beDesiredGoals').value
-    );
-  } else if (
-    document.getElementById('beDesiredCriteria').value === '1st Half'
-  ) {
-    desiredAvg1HCondition = document.getElementById('beDesiredComp').value;
-    desiredAvg1H = getNumValue(document.getElementById('beDesiredGoals').value);
-  } else if (
-    document.getElementById('beDesiredCriteria').value === '2nd Half'
-  ) {
-    desiredAvg2HCondition = document.getElementById('beDesiredComp').value;
-    desiredAvg2H = getNumValue(document.getElementById('beDesiredGoals').value);
-  } else if (document.getElementById('beDesiredCriteria').value === 'Home') {
-    desiredAvgGoalsHomeCondition =
-      document.getElementById('beDesiredComp').value;
-    desiredAvgGoalsHome = getNumValue(
-      document.getElementById('beDesiredGoals').value
-    );
-  } else {
-    desiredAvgGoalsAwayCondition =
-      document.getElementById('beDesiredComp').value;
-    desiredAvgGoalsAway = getNumValue(
-      document.getElementById('beDesiredGoals').value
-    );
-  }
-
-  console.log('leagueId: ', leagueId);
-  const options = {
-    avgGoalsH: avgGoalsH,
-    avgGoalsHCondition: avgGoalsHCondition,
-    avgGoalsA: avgGoalsA,
-    avgGoalsACondition: avgGoalsACondition,
-    avgGoalsHome: avgGoalsHome,
-    avgGoalsHomeCondition: avgGoalsHomeCondition,
-    avgGoalsAway: avgGoalsAway,
-    avgGoalsAwayCondition: avgGoalsAwayCondition,
-    avgScoredHome: avgScoredHome,
-    avgScoredHomeCondition: avgScoredHomeCondition,
-    avgConcededHome: avgConcededHome,
-    avgConcededHomeCondition: avgConcededHomeCondition,
-    avgScoredAway: avgScoredAway,
-    avgScoredAwayCondition: avgScoredAwayCondition,
-    avgConcededAway: avgConcededAway,
-    avgConcededAwayCondition: avgConcededAwayCondition,
-    avg1HHome: avg1HHome,
-    avg1HHomeCondition: avg1HHomeCondition,
-    avg2HHome: avg2HHome,
-    avg2HHomeCondition: avg2HHomeCondition,
-    avg1HAway: avg1HAway,
-    avg1HAwayCondition: avg1HAwayCondition,
-    avg2HAway: avg2HAway,
-    avg2HAwayCondition: avg2HAwayCondition,
+    ),
+    desiredCriteria: document.getElementById('beDesiredCriteria').value,
+    desiredComp: document.getElementById('beDesiredComp').value,
+    desiredGoals: getNumValue(document.getElementById('beDesiredGoals').value),
   };
-  const desiredOutcomeOptions = {
-    desiredAvgGoalsInGeneral: desiredAvgGoalsInGeneral,
-    desiredAvgGoalsInGeneralCondition: desiredAvgGoalsInGeneralCondition,
-    desiredAvgGoalsHome: desiredAvgGoalsHome,
-    desiredAvgGoalsHomeCondition: desiredAvgGoalsHomeCondition,
-    desiredAvgGoalsAway: desiredAvgGoalsAway,
-    desiredAvgGoalsAwayCondition: desiredAvgGoalsAwayCondition,
-    desiredAvg1H: desiredAvg1H,
-    desiredAvg1HCondition: desiredAvg1HCondition,
-    desiredAvg2H: desiredAvg2H,
-    desiredAvg2HCondition: desiredAvg2HCondition,
-  };
+
+  const leagueId = getNumValue(elements.league);
+  // HARDCODED
+  const seasonId = leagueId === 271 ? 21644 : 21787;
+
+  const options = getOptions(elements);
+  const desiredOutcomeOptions = getDesiredOutcomeOptions(elements);
 
   const result = filterMatches(
     leagueId,
@@ -170,13 +207,7 @@ function beAlgo() {
     desiredOutcomeOptions
   );
 
-  if (result) {
-    resultElement.innerText = `${result} %`;
-    resultElement.classList.remove('inactive');
-  } else {
-    resultElement.innerText = '0%';
-    resultElement.classList.remove('inactive');
-  }
+  displayResult(result, elements.resultElement);
 }
 
 // Attach event listener to the button
